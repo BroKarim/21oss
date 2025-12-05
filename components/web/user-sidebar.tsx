adb6644a4504f356d88b1ffe393df8b96d27c9b5
BroKarim <bukutulis0110@gmail.com>
2025-12-05 11:10:16 +0700

    feat: improve copy-to-clipboard and add confirmation

--- components/web/user-sidebar.tsx
+++
@@ -1,8 +0,0 @@
-import { findAd } from "@/server/web/ads/queries";
-import { NavLayout } from "@/components/web/main-page/nav-layout"; 
-
-export async function UserSidebar() {
-  const ad = await findAd({ where: { type: "SidebarBanner" } });
-
-  return <NavLayout ad={ad} />;
-}
