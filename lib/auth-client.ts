463c9cf821ad074798f85d56c606ef6beb4ecbd8
BroKarim <bukutulis0110@gmail.com>
2026-04-15 10:00:33 +0700

    fix(auth): update auth-client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,8 +1,7 @@
 import { adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
-import { env } from "@/env";
 
 export const { signIn, signOut, useSession, admin } = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_SITE_URL,
+  baseURL: "",
   plugins: [adminClient()],
 });
